7d419ad081c1fd86bf026c0676848a89031ddf57
Bing <bing@zkpass.org>
2026-01-15 01:20:13 +0900

    change time

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,8 @@
 const taskTimestamps = {
-  task1StartTime: 1768406100,
-  task1EndTime: 1768406520,
-  task2StartTime: 1768406700,
-  task2EndTime: 1768407120
+  task1StartTime: 1768407900,
+  task1EndTime: 1768408320,
+  task2StartTime: 1768408500,
+  task2EndTime: 1768408920
 }
 
 const DELEGATE_AMOUNT_1 = 1;
