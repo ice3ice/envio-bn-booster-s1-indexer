44af7eb9b5a628e2da82bf11a27ae19df98a263a
Bing <bing@zkpass.org>
2026-01-19 23:16:15 +0900

    change time

--- src/utils.js
+++ src/utils.js
@@ -9,9 +9,11 @@ const DELEGATE_AMOUNT_1 = 1;
 const DELEGATE_AMOUNT_2 = 2;
 
 const getTaskCompleted = (timestamp, amount, userTaskCompleted) => {
+  // Task 1
   if(timestamp >= taskTimestamps.task1StartTime && timestamp <= taskTimestamps.task1EndTime && amount >= DELEGATE_AMOUNT_1) {
     userTaskCompleted.task1Completed = true;
     return userTaskCompleted;
+  // Task 2
   } else if(timestamp >= taskTimestamps.task2StartTime && timestamp <= taskTimestamps.task2EndTime && amount >= DELEGATE_AMOUNT_2) {
     userTaskCompleted.task2Completed = true;
     return userTaskCompleted;
